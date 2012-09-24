cca5cca7852e5614f285efb0c90149835b3a4d63
Marcio Sagaz <marciosagaz@yahoo.com.br>
2012-09-24 19:56:09 -0300

    Instalando corretamente as biblioteca

--- TwitterProject/src/Main.java
+++ TwitterProject/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 // The factory instance is re-useable and thread safe.
+	    Twitter twitter = new TwitterFactory().getInstance();
+	    Status status = twitter.updateStatus(latestStatus);
+	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
 	}
 
 }
