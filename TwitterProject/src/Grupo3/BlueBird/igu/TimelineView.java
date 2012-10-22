c445c16d92b9c1c3b2c1db9996f9053dbfd6a4b3
Marcio <Marcio@Marcio-Note>
2012-10-21 23:33:22 -0200

    Nova classe timeline adicionar ao Painel principal

---
+++ TwitterProject/src/Grupo3/BlueBird/igu/TimelineView.java
@@ -0,0 +1,61 @@
+package Grupo3.BlueBird.igu;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import twitter4j.Status;
+
+public class TimelineView {
+	
+	private Container _container;
+	private JTextArea _textArea;
+	
+	public TimelineView(int linha, int coluna) {
+		
+		_container = new JPanel(new BorderLayout());
+		_textArea = new JTextArea(linha, coluna);
+		JScrollPane scrollPane = new JScrollPane(_textArea);
+		_container.add(scrollPane, BorderLayout.CENTER);
+		_textArea.setEditable(false);
+	}
+	
+	public Container getContainer(){
+		return _container;
+	}
+
+	public void printStatus(List<Status> statuses){
+		_textArea.setEditable(true);
+		_textArea.setText("");
+		for (Status status : statuses) {
+          _textArea.append("@" + status.getUser().getScreenName() + "\n");
+          String text = status.getText();
+          int limite = 50;
+          int limite2 = 100;
+          if (text.length() < 50) {
+        	  _textArea.append( text + "\n\n");
+          }else if (text.length() < 100){
+        	  while(text.substring(limite-1,limite).compareTo(" ") != 0){
+        		  limite--;
+        	  }
+        	  _textArea.append( text.substring(0,limite) + "\n");
+        	  _textArea.append( text.substring(limite,text.length()-1) + "\n\n");
+          }else {
+        	  while(text.substring(limite-1,limite).compareTo(" ") != 0){
+        		  limite--;
+        	  }
+        	  while(text.substring(limite2-1,limite2).compareTo(" ") != 0){
+        		  limite2--;
+        	  }
+        	  _textArea.append( text.substring(0,limite) + "\n");
+        	  _textArea.append( text.substring(limite,limite2) + "\n");
+        	  _textArea.append( text.substring(limite2,text.length()-1) + "\n\n");
+          }
+		}
+		_textArea.setEditable(false);
+	}
+}
--- TwitterProject/src/Grupo3/BlueBird/logica/Timeline.java
+++ TwitterProject/src/Grupo3/BlueBird/logica/Timeline.java
@@ -2,31 +2,44 @@ package Grupo3.BlueBird.logica;
 
 import java.util.List;
 
+import Grupo3.BlueBird.igu.TimelineView;
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.User;
 
 public class Timeline {
 	
-	private Twitter twitter;
+	private Twitter _twitter;
+	private List<Status> _statuses;
+	private TimelineView _currentTimelineView;
 
 		public Timeline(Twitter twitter) {
-			this.twitter = twitter;
+			_twitter = twitter;
 		}
 		
-		public List<Status> getTimeline(){
+		public Boolean refreshHomeTimeline(){
 	        try {
-	            User user = twitter.verifyCredentials();
-	            List<Status> statuses = twitter.getHomeTimeline();
-	            System.out.println("Showing @" + user.getScreenName() + "'s home timeline.");
-	            for (Status status : statuses) {
-	                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-	            }
-	            return statuses;
+	            _statuses = _twitter.getHomeTimeline();
+	            _currentTimelineView.printStatus(_statuses);
+	            return true;
 	        } catch (TwitterException te) {
 	            te.printStackTrace();
-	            return null;
+	            return false;
 	        }
 	    }
+		
+		public List<Status> getTimeline(){
+			return _statuses;
+		}
+		
+		public void setCurrentTimelineView(TimelineView tlv){
+			_currentTimelineView = tlv;
+		}
+		
+		public TimelineView getCurrentTimelineView(){
+			return _currentTimelineView;
+		}
+		
+		
 }
